140844cfa2726438e184e8f297c3b131fa0cea7d
Chris Beck <beck.ct@gmail.com>
2023-03-22 16:55:20 -0600

    add example client

---
+++ client/src/config.rs
@@ -0,0 +1,96 @@
+// Copyright (c) 2018-2023 The MobileCoin Foundation
+#![deny(missing_docs)]
+
+//! Configuration parameters for the example BOMB client
+
+use clap::{Parser, Subcommand};
+use mc_bomb_uri::BombUri;
+use mc_util_parse::parse_hex;
+
+/// Configuration parameters for the example BOMB client
+#[derive(Clone, Debug, Parser)]
+#[clap(version)]
+pub struct BombClientConfig {
+    /// URI for bomb server
+    #[clap(long, env = "MC_URI")]
+    pub uri: Option<BombUri>,
+
+    /// Secret key which we use to authenticate.
+    #[clap(long, value_parser = parse_hex::<[u8; 32]>, env = "MC_SECRET_KEY")]
+    pub secret_key: Option<[u8; 32]>,
+
+    /// The command to run.
+    #[clap(subcommand)]
+    pub cmd: BombClientCommand,
+}
+
+/// The command to run.
+#[derive(Clone, Debug, Subcommand)]
+pub enum BombClientCommand {
+    /// Generate a private / public hex key pair
+    GenerateKey,
+
+    /// Show public key hex corresponding to our secret key
+    ShowPublicKey,
+
+    /// Create a record in the bomb server
+    Create {
+        /// The id of the message we wish to create. Will be chosen randomly if
+        /// not provided.
+        #[clap(long, value_parser = parse_hex::<[u8; 16]>, env = "MC_MSG_ID")]
+        msg_id: Option<[u8; 16]>,
+
+        /// The recipient of the message.
+        #[clap(long, value_parser = parse_hex::<[u8; 32]>, env = "MC_RECIPIENT")]
+        recipient: [u8; 32],
+
+        /// The text message we wish to send.
+        #[clap(long, env = "MC_MESSAGE")]
+        message: String,
+    },
+
+    /// Read a record in the bomb server
+    Read {
+        /// The id of the message we wish to read. If not provided, the server
+        /// will return an arbitrary message directed at us, if any exists.
+        ///
+        /// We will only be able to read the message if we were the sender or
+        /// the recipient.
+        #[clap(long, value_parser = parse_hex::<[u8; 16]>, env = "MC_MSG_ID")]
+        msg_id: Option<[u8; 16]>,
+    },
+
+    /// Update a record in the bomb server
+    Update {
+        /// The id of the message we wish to update.
+        #[clap(long, value_parser = parse_hex::<[u8; 16]>, env = "MC_MSG_ID")]
+        msg_id: [u8; 16],
+
+        /// The recipient of the message.
+        /// Update will fail if this is not correct.
+        /// (This is needed in order to update the timestamp also, in the other
+        /// table. We cannot access the tables in the other order if we
+        /// want to have the same access pattern as a READ operation)
+        #[clap(long, value_parser = parse_hex::<[u8; 32]>, env = "MC_RECIPIENT")]
+        recipient: [u8; 32],
+
+        /// The new message contents.
+        #[clap(long, env = "MC_MESSAGE")]
+        message: String,
+    },
+
+    /// Delete a record in the bomb server
+    Delete {
+        /// The id of the message we wish to delete.
+        #[clap(long, value_parser = parse_hex::<[u8; 16]>, env = "MC_MSG_ID")]
+        msg_id: [u8; 16],
+
+        /// The recipient of the message.
+        /// Delete will fail if this is not correct.
+        /// (This is needed in order to delete the record in the other table as
+        /// well. We cannot access the tables in the other order if we
+        /// want to have the same access pattern as a READ operation)
+        #[clap(long, value_parser = parse_hex::<[u8; 32]>, env = "MC_RECIPIENT")]
+        recipient: [u8; 32],
+    },
+}
---
+++ client/src/main.rs
@@ -0,0 +1,190 @@
+// Copyright (c) 2018-2023 The MobileCoin Foundation
+
+//! Example BOMB client
+
+use chrono::{DateTime, NaiveDateTime, Utc};
+use mc_attest_verifier::{Verifier, DEBUG_ENCLAVE};
+use mc_bomb_connection::{BombGrpcConnection, Error, PAYLOAD_SIZE};
+use mc_bomb_types::{QueryResponse, STATUS_CODE_NOT_FOUND, STATUS_CODE_SUCCESS};
+use mc_common::logger::{create_root_logger, log, Logger};
+use mc_crypto_keys::{CompressedRistrettoPublic, RistrettoPrivate, RistrettoPublic};
+use mc_crypto_rand::McRng;
+use mc_util_cli::ParserWithBuildInfo;
+use mc_util_from_random::FromRandom;
+use mc_util_grpc::GrpcRetryConfig;
+use std::{process::exit, sync::Arc};
+
+mod config;
+use config::{BombClientCommand, BombClientConfig};
+
+fn main() {
+    // Logging must go to stderr to not interfere with STDOUT
+    std::env::set_var("MC_LOG_STDERR", "1");
+
+    let logger = create_root_logger();
+
+    let config = BombClientConfig::parse();
+
+    if let Err(err) = do_work(config, logger) {
+        eprintln!("Error: {}", err);
+        exit(1);
+    }
+    eprintln!("Success");
+}
+
+fn do_work(config: BombClientConfig, logger: Logger) -> Result<(), Error> {
+    let get_conn = || -> BombGrpcConnection<RistrettoPrivate> {
+        let uri = config
+            .uri
+            .clone()
+            .expect("uri is required for this operation");
+        let secret_key = RistrettoPrivate::try_from(
+            config
+                .secret_key
+                .as_ref()
+                .expect("secret key required for this operation"),
+        )
+        .expect("invalid secret key");
+
+        let grpcio_env = Arc::new(grpcio::EnvBuilder::new().build());
+
+        let retry_config = GrpcRetryConfig::default();
+
+        let mut verifier = Verifier::default();
+        let mr_enclave_verifier = mc_bomb_enclave_measurement::get_mr_enclave_verifier();
+        verifier
+            .debug(DEBUG_ENCLAVE)
+            .mr_enclave(mr_enclave_verifier);
+
+        log::debug!(logger, "Attestation verifier: {:?}", &verifier);
+        BombGrpcConnection::new(
+            uri,
+            retry_config,
+            secret_key,
+            verifier,
+            grpcio_env,
+            logger.clone(),
+        )
+    };
+
+    let mut rng = McRng::default();
+
+    match config.cmd {
+        BombClientCommand::GenerateKey => {
+            let secret_key = RistrettoPrivate::from_random(&mut rng);
+
+            println!("secret key hex: \"{}\"", hex::encode(secret_key.to_bytes()));
+
+            let public_key = CompressedRistrettoPublic::from(RistrettoPublic::from(&secret_key));
+
+            println!("public key hex: \"{}\"", hex::encode(public_key.as_bytes()));
+        }
+        BombClientCommand::ShowPublicKey => {
+            let secret_key = RistrettoPrivate::try_from(
+                config
+                    .secret_key
+                    .as_ref()
+                    .expect("secret key required for this operation"),
+            )
+            .expect("invalid secret key");
+
+            let public_key = CompressedRistrettoPublic::from(RistrettoPublic::from(&secret_key));
+
+            println!("public key hex: \"{}\"", hex::encode(public_key.as_bytes()));
+        }
+        BombClientCommand::Create {
+            msg_id,
+            recipient,
+            message,
+        } => {
+            let msg_id = msg_id.unwrap_or(FromRandom::from_random(&mut rng));
+            let recipient = CompressedRistrettoPublic::from(&recipient);
+            let message = string_to_payload(&message);
+
+            let mut conn = get_conn();
+            let resp = conn.create(&msg_id, &recipient, &message)?;
+            pretty_print_response(&resp);
+        }
+        BombClientCommand::Read { msg_id } => {
+            let msg_id = msg_id.unwrap_or([0u8; 16]);
+
+            let mut conn = get_conn();
+            let resp = conn.read(&msg_id)?;
+            pretty_print_response(&resp);
+        }
+
+        BombClientCommand::Update {
+            msg_id,
+            recipient,
+            message,
+        } => {
+            let recipient = CompressedRistrettoPublic::from(&recipient);
+            let message = string_to_payload(&message);
+
+            let mut conn = get_conn();
+            let resp = conn.update(&msg_id, &recipient, &message)?;
+            pretty_print_response(&resp);
+        }
+
+        BombClientCommand::Delete { msg_id, recipient } => {
+            let recipient = CompressedRistrettoPublic::from(&recipient);
+
+            let mut conn = get_conn();
+            let resp = conn.delete(&msg_id, &recipient)?;
+            pretty_print_response(&resp);
+        }
+    }
+    Ok(())
+}
+
+// Encode a string to the fixed payload size, using framing and zero padding,
+// which the payload schema this example app uses.
+fn string_to_payload(string_data: &str) -> Vec<u8> {
+    assert!(string_data.len() <= PAYLOAD_SIZE - 2, "string too long");
+    let mut result = vec![0u8; PAYLOAD_SIZE];
+    (&mut result[0..2]).copy_from_slice(&(string_data.len() as u16).to_le_bytes()[..]);
+    (&mut result[2..(2 + string_data.len())]).copy_from_slice(string_data.as_bytes());
+    result
+}
+
+// Decode a string from the framed payload format.
+fn payload_to_string(payload_data: &[u8]) -> String {
+    assert_eq!(payload_data.len(), PAYLOAD_SIZE);
+    let length = u16::from_le_bytes(payload_data[0..2].try_into().unwrap()) as usize;
+
+    assert!(
+        length <= PAYLOAD_SIZE - 2,
+        "recorded encoding length is out of bounds"
+    );
+    std::str::from_utf8(&payload_data[2..2 + length])
+        .expect("utf-8 decoding error")
+        .to_owned()
+}
+
+fn pretty_print_response(resp: &QueryResponse) -> String {
+    use serde_json::{json, to_string_pretty};
+
+    let status_code_string = match resp.status_code {
+        STATUS_CODE_SUCCESS => "SUCCESS".to_string(),
+        STATUS_CODE_NOT_FOUND => "NOT_FOUND".to_string(),
+        other => other.to_string(),
+    };
+
+    let time_string = NaiveDateTime::from_timestamp_opt(resp.record.timestamp as i64, 0)
+        .map(|naive| DateTime::<Utc>::from_utc(naive, Utc).to_string())
+        .unwrap_or_else(|| "???".to_string());
+
+    let record = json!({
+        "msg_id": hex::encode(&resp.record.msg_id),
+        "sender": hex::encode(&resp.record.sender),
+        "recipient": hex::encode(&resp.record.recipient),
+        "timestamp": time_string,
+        "payload": payload_to_string(&resp.record.payload),
+    });
+
+    to_string_pretty(&json!({
+        "record": record,
+        "status_code": status_code_string,
+    }))
+    .expect("pretty printing failed")
+}
